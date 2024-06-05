e126fe1e2e5a306b0f6fd61738bc8987d1288383
Sidharth Mudgil <68889544+SidharthMudgil@users.noreply.github.com>
2024-06-06 00:15:13 +0530

    finalize dashboard

--- lib/presentation/pages/home/home_screen.dart
+++ lib/presentation/pages/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:touristic/core/constants/constants.dart';
 import 'package:touristic/presentation/widgets/item_card_dashboard.dart';
@@ -13,6 +14,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _selected = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,21 +34,80 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-    Widget _dashboard() {
-      return Container(
-        color: AppTheme.blue80,
-        child: ListView(
-          children: [
-            ...dashboardItems.entries.map((entry) {
-              return ItemCardDashboard(
-                title: entry.key,
-                iconData: entry.value,
-              );
-            }).toList()
-          ],
-        ),
+  Widget _dashboard() {
+    final mediaQuery = MediaQuery.of(context);
+    final list = _getDashboardItems();
+
+    return Container(
+      color: AppTheme.blue80,
+      padding: EdgeInsets.only(top: mediaQuery.viewPadding.top),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SizedBox(height: 10,),
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: Text(
+              "Liquid Galaxy",
+              style: TextStyle(
+                color: AppTheme.gray10,
+                fontFamily: "Noto",
+                fontSize: 14,
+                fontWeight: FontWeight.w800
+              ),
+              textAlign: TextAlign.start,
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: Text(
+              "Touristic AI",
+              style: TextStyle(
+                color: AppTheme.gray10,
+                fontFamily: "Noto",
+                  fontSize: 20,
+                  fontWeight: FontWeight.w800
+              ),
+              textAlign: TextAlign.start,
+            ),
+          ),
+
+          SizedBox(height: 10,),
+          ..._getDashboardItems()
+        ],
+      ),
+    );
+  }
+
+  List<Widget> _getDashboardItems() {
+    final List<Widget> items = [];
+    final list = dashboardItems.entries.toList();
+
+    for (int i = 0; i < dashboardItems.length; i++) {
+      items.add(
+          Material(
+            color: AppTheme.blue80,
+            child: InkWell(
+              splashColor: AppTheme.blue70,
+              onTap: () {
+                setState(() {
+                  _selected = i;
+                });
+              },
+              child: ItemCardDashboard(
+                title: list[i].key,
+                iconData: list[i].value,
+                selected: _selected == i,
+              ),
+            ),
+          )
       );
     }
+
+    return items;
+  }
 
   Widget _content() {
     return const Navigator(
--- lib/presentation/widgets/item_card_dashboard.dart
+++ lib/presentation/widgets/item_card_dashboard.dart
@@ -4,50 +4,47 @@ import 'package:touristic/config/theme/app_theme.dart';
 class ItemCardDashboard extends StatelessWidget {
   final String title;
   final IconData iconData;
+  final bool selected;
 
   const ItemCardDashboard({
     super.key,
     required this.title,
     required this.iconData,
+    required this.selected,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          verticalDirection: VerticalDirection.up,
-          children: [
-            Container(
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                color: AppTheme.blue70,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Icon(
-                iconData,
-                size: 18,
-                color: AppTheme.gray10,
-              ),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        verticalDirection: VerticalDirection.up,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: selected ? AppTheme.purple40 : AppTheme.blue70,
+              borderRadius: BorderRadius.circular(10),
             ),
-            const SizedBox(
-              width: 10,
+            child: Icon(
+              iconData,
+              size: 18,
+              color: selected ? AppTheme.gray10 : AppTheme.gray10,
             ),
-            Text(
-              title,
-              style: TextStyle(
-                color: AppTheme.gray10,
-                fontFamily: "Noto",
-                fontWeight: FontWeight.w700,
-              ),
-            )
-          ],
-        ),
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          Text(
+            title,
+            style: TextStyle(
+              color: selected ? AppTheme.gray10 : AppTheme.gray30,
+              fontFamily: "Noto",
+              fontWeight: FontWeight.w700,
+            ),
+          )
+        ],
       ),
     );
   }
