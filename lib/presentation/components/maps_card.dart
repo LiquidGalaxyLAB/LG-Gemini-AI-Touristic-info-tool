11157a1146547309e315cf57a3368bbf1097b587
Sidharth Mudgil <68889544+SidharthMudgil@users.noreply.github.com>
2024-07-02 01:25:04 +0530

    add maps syncing logic

--- lib/presentation/components/maps_card.dart
+++ lib/presentation/components/maps_card.dart
@@ -4,6 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import '../../config/theme/app_theme.dart';
+import '../../core/enums/preferences.dart';
+import '../../core/utils/preferences_utils.dart';
+import '../../service/lg_service.dart';
 
 class MapsCard extends StatefulWidget {
   final Completer<GoogleMapController> _controller;
@@ -20,6 +23,31 @@ class MapsCard extends StatefulWidget {
 }
 
 class MapsCardState extends State<MapsCard> {
+  LGService? lgService;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeMembers();
+  }
+
+  void _initializeMembers() async {
+    final PreferencesUtils preferencesUtils = PreferencesUtils();
+    String username = (await preferencesUtils.getValue(ConnectionPreferences.username.name)) ?? "";
+    String password = (await preferencesUtils.getValue(ConnectionPreferences.password.name)) ?? "";
+    String ip = (await preferencesUtils.getValue(ConnectionPreferences.ip.name)) ?? "";
+    String port = (await preferencesUtils.getValue(ConnectionPreferences.port.name)) ?? "0";
+    String screens = (await preferencesUtils.getValue(ConnectionPreferences.screens.name)) ?? "0";
+
+    lgService = LGService(
+      host: ip,
+      port: int.parse(port),
+      username: username,
+      password: password,
+      slaves: int.parse(screens),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
@@ -32,7 +60,7 @@ class MapsCardState extends State<MapsCard> {
           widget._controller.complete(controller);
         },
         onCameraMove: (cameraPosition) {
-
+          lgService?.flyTo(cameraPosition);
         },
       ),
     );
