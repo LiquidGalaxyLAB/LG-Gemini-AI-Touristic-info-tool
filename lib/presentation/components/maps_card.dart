46635a336611ad1a6ff0d1d8f496acb9a72bc22c
Sidharth Mudgil <68889544+SidharthMudgil@users.noreply.github.com>
2024-07-11 22:06:22 +0530

    add option to add markers and more on map

--- lib/presentation/components/maps_card.dart
+++ lib/presentation/components/maps_card.dart
@@ -9,11 +9,24 @@ import '../../service/lg_service.dart';
 class MapsCard extends StatefulWidget {
   final Completer<GoogleMapController> _controller;
   final CameraPosition _cameraPosition;
+  final Set<Marker> _markers;
+  final Set<Polygon> _polygons;
+  final Set<Polyline> _polylines;
+  final Set<Circle> _circles;
+
   const MapsCard({
     super.key,
     required Completer<GoogleMapController> controller,
     required CameraPosition cameraPosition,
-  })  : _cameraPosition = cameraPosition,
+    Set<Marker> markers = const {},
+    Set<Polygon> polygons = const {},
+    Set<Polyline> polylines = const {},
+    Set<Circle> circles = const {},
+  })  : _markers = markers,
+        _circles = circles,
+        _polygons = polygons,
+        _polylines = polylines,
+        _cameraPosition = cameraPosition,
         _controller = controller;
 
   @override
@@ -21,12 +34,18 @@ class MapsCard extends StatefulWidget {
 }
 
 class MapsCardState extends State<MapsCard> {
-
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: const BorderRadius.all(Radius.circular(10)),
       child: GoogleMap(
+        markers: widget._markers,
+        polygons: widget._polygons,
+        polylines: widget._polylines,
+        circles: widget._circles,
+        zoomControlsEnabled: false,
+        zoomGesturesEnabled: true,
+        tiltGesturesEnabled: true,
         mapType: AppTheme.mapStyle,
         style: AppTheme.mapTheme,
         initialCameraPosition: widget._cameraPosition,
